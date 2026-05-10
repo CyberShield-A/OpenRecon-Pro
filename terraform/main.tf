b2c45ddac09556b1a67c901df14f6d48a2ba6e29
TORSOOEtehG <etehgnadewdinatorsoo@gmail.com>
2026-05-10 23:55:55 +0000

    Fix: Update Terraform provider and add resource limits

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,45 @@
-# Indispensable pour que Terraform sache quelle image utiliser
-resource "docker_image" "openrecon_img" {
+terraform {
+  required_providers {
+    docker = {
+      # Correction vitale : source officielle actuelle
+      source  = "kreuzwerker/docker"
+      version = "~> 3.0.1"
+    }
+  }
+}
+
+provider "docker" {
+  # Connexion au socket Docker local de ta VM Ubuntu
+  host = "unix:///var/run/docker.sock"
+}
+
+# Référence à l'image que Jenkins vient de construire
+resource "docker_image" "openrecon_app" {
   name         = "openrecon-app:latest"
   keep_locally = true
 }
 
-resource "docker_container" "openrecon_container" {
+# Déploiement du conteneur
+resource "docker_container" "openrecon_service" {
+  image = docker_image.openrecon_app.image_id
   name  = "openrecon-service"
-  image = docker_image.openrecon_img.image_id
   
+  # Mappage des ports : 8081 (externe) -> 5000 (interne Flask)
   ports {
-    internal = 5000 
-    external = 8081 
+    internal = 5000
+    external = 8081
   }
 
-  memory = 1024 
+  # Limites de ressources (Bonne pratique DevOps/Cyber)
+  memory = 1024 # Limite à 1 Go de RAM
+  cpu_shares = 512
 
-  restart  = "always"
-  must_run = true
+  # Redémarrage automatique si le service crash
+  restart = "always"
+
+  # Variable d'environnement pour Flask (double sécurité)
+  env = [
+    "FLASK_ENV=production",
+    "PYTHONUNBUFFERED=1"
+  ]
 }
