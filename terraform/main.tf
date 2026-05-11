10f78d8269705bbaaed571dceab01059c731af1e
TORSOOEtehG <etehgnadewdinatorsoo@gmail.com>
2026-05-11 00:04:01 +0000

    Fix: stable infrastructure using data source for docker image

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 terraform {
   required_providers {
     docker = {
-      # Correction vitale : source officielle actuelle
       source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
@@ -9,35 +8,28 @@ terraform {
 }
 
 provider "docker" {
-  # Connexion au socket Docker local de ta VM Ubuntu
   host = "unix:///var/run/docker.sock"
 }
 
-# Référence à l'image que Jenkins vient de construire
-resource "docker_image" "openrecon_app" {
-  name         = "openrecon-app:latest"
-  keep_locally = true
+# Au lieu de "resource", on utilise "data" pour lire l'image existante
+# Cela évite que Terraform essaie de la recréer ou de la supprimer
+data "docker_image" "openrecon_app" {
+  name = "openrecon-app:latest"
 }
 
-# Déploiement du conteneur
 resource "docker_container" "openrecon_service" {
-  image = docker_image.openrecon_app.image_id
+  # On utilise l'ID récupéré par le bloc data
+  image = data.docker_image.openrecon_app.id
   name  = "openrecon-service"
   
-  # Mappage des ports : 8081 (externe) -> 5000 (interne Flask)
   ports {
     internal = 5000
     external = 8081
   }
 
-  # Limites de ressources (Bonne pratique DevOps/Cyber)
-  memory = 1024 # Limite à 1 Go de RAM
-  cpu_shares = 512
-
-  # Redémarrage automatique si le service crash
+  memory = 1024
   restart = "always"
 
-  # Variable d'environnement pour Flask (double sécurité)
   env = [
     "FLASK_ENV=production",
     "PYTHONUNBUFFERED=1"
